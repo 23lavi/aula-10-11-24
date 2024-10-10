88b38bb3bef15dd12a34b9a7411223137569044c
EA - Lavinia Reis Conceição <lavinia_reis@educadventista.org>
2024-10-10 14:05:56 -0300

    arrumando

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function somar(){
     const numero1 = Number(document.querySelector("#numero-1").value)
     const numero2 = Number(document.querySelector("#numero-2").value)
-    const numero3 = Number(document.querySelector("#numero-3").value)
 
     const paragrafo = document.querySelector(".resultado")
 
-    paragrafo.innerHTML = `resultado: ${numero1 + numero2 + numero3}`
+    paragrafo.innerHTML = `resultado: ${numero1+numero2}`
 
 } 
 
